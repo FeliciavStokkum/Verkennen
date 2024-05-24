e0071bf6d953be2b9f95160982affa1f6e181e52
FeliciavStokkum <143702697+FeliciavStokkum@users.noreply.github.com>
2024-05-24 10:16:59 +0200

    cookie_jar

--- Module_3/Deel_3/exercises/cookie_jar/script.js
+++ Module_3/Deel_3/exercises/cookie_jar/script.js
@@ -1,39 +1,55 @@
 let container = document.getElementById('container');
 
+
 function create_button(soortKnop){
     // creeer een standaard button element
     const knop = document.createElement('button');
     knop.type = 'button';
+    knop.textContent = soortKnop
     // maak een switch of if statement die de specifieke elementen van een button zet
-    if (soortKnop === 'add'){
-        soortKnop.innerHTML = 'Add cookies';
-    elif (soortKnop === 'take')
-        soortKnop.innerHTML = 'Take cookie';
-    elif (soortKnop === 'place')
-        soortKnop.innerHTML = 'Place lid';
-    }
+    switch(soortKnop) {
+        case 'add':
+          // code block
+          knop.textContent = 'Vul met cookies'
+          knop.addEventListener('click', proccess_addcookies_click);
+          break;
+        case 'take':
+          // code block
+          knop.textContent = 'Pak een cookie'
+          knop.addEventListener('click', proccess_takecookie_click);
+          break;
+        default:
+          // code block
+          knop.textContent = 'Plaats de deksel'
+      }
     // return het button element
     return knop;
 }
 
-function proccess_takecookie_click(){ 
-    let jar; //vul deze variable met het element cookie-jar
+function proccess_takecookie_click(){
+    let jar = document.getElementById('cookie-jar');
+    let currentcookies; 
 
+    jar.removeChild(jar.firstChild);
+    }
+    //vul deze variable met het element cookie-jar
+    // vul deze variablelen met het huidige aantal cookies in de jar 
     // maak een if statement die kijkt hoeveel cookies er nog zijn
     // bij 1 cookie wordt de vraag gesteld: 'Weet u het zeker om de laatste te pakken?'
     // bij 0 wordt de melding gegeven: 'Geen cookies meer' 
 
     // als er een cookie gepakt is moet je hier de bovenste verwijderen
-}
 
 function proccess_addcookies_click() {
-    let jar; //vul deze variable met het element cookie-jar
+    let jar = document.getElementById('cookie-jar'); //vul deze variable met het element cookie-jar
     let currentcookies; // vul deze variablelen met het huidige aantal cookies in de jar 
-
-    let repeat = true;
+    cookie = create_cookie()
+    jar.appendChild(cookie)
+    let repeat = false;
+    return
     while (repeat){
         // vraag hoeveel cookies iemand wil toevoegen
-        
+        create_cookie()
         // reken uit hoeveel cookies het totaal zou worden
 
         repeat; // vul deze variablen met een boolean die true is als het aantal gevraagde cookies + aantal toegevoegde cookies groter is dan 7
@@ -42,7 +58,6 @@ function proccess_addcookies_click() {
             // geef hier het bericht naar de gebruiker aan wat het maximale aantal cookies is dat ingevuld mocht worden.
         }
     }
-
     // creeer het aantal gevraagde cookies met een for loop, let wel op dat je deze steeds bovenop plaatst
 }
 
@@ -63,7 +78,8 @@ function proccess_placelit_click() {
 
 // zorg dat het nummer van iedere cookie wordt weergegeven
 function create_cookie(){
-    cookie = document.createElement('div');
+    console.log('create cookie')
+    const cookie = document.createElement('div');
     cookie.classList.add('cookie');
     cookie.innerHTML = 'Cookie';
 
